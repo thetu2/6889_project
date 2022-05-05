40784594a59c59709a598f876e8df375cd4a1413
thetu2 <44556189+thetu2@users.noreply.github.com>
2022-05-05 19:09:28 -0400

    Added additional graphs

--- display.py
+++ display.py
@@ -52,7 +52,10 @@ window_start_time=datetime.datetime(2000,1,1,0,0,0,0) #initialize window start t
 curr_window_rating={}
 curr_window_rating_followers={}
 window_rating_avg=[0.0]*len(movies)
-cum_avg={}#start in the middle
+cum_avg={}
+temp_avg={}
+volume_trend={} #tracks # of tweets about movie in all time windows
+volume_window={} #tracks # of tweets about movie in the given time window
 cum_x=[]
 
 #init storage elements
@@ -62,10 +65,13 @@ for movie in movies:
     curr_window_rating[movie]=[] #current list of ratings in window
     curr_window_rating_followers[movie] = []  # # of follower for current list of ratings in window
     cum_avg[movie]=[] #initialize average as 5.0 for each movie
+    temp_avg[movie] = []  # initialize average as 5.0 for each movie
+    volume_trend[movie] = []  # initialize average as 5.0 for each movie
+    volume_window[movie]=0
 
 
 #figure setup
-fig = plt.figure(figsize=(21,6))
+fig = plt.figure(figsize=(20,13))
 color_batman=['black']*4
 color_fant=['grey']*4
 color_morbius=['red']*4
@@ -129,17 +135,23 @@ for _ in range(500):
 
             curr_window_rating_followers[movie] = []  # clear follower list
             # Calculate cumulative movie score
-            if (not cum_avg[movie]):
+            if (not cum_avg[movie]): #initial condition
                 cum_avg[movie].append(5.0)
-            elif (not curr_window_rating[movie]): # not ratings in current time window
+                temp_avg[movie].append(0.0)
+            elif (not curr_window_rating[movie]): # no ratings in current time window
                 cum_avg[movie].append(cum_avg[movie][-1])
+                temp_avg[movie].append(temp_avg[movie][-1])
             else:
                 cum_avg[movie].append(cum_avg[movie][-1]*0.8+0.2*window_rating_avg[idx])
+                temp_avg[movie].append(window_rating_avg[idx])
             curr_window_rating[movie] = []  # clear movie ratings list
+            volume_trend[movie].append(volume_window[movie]) #save tweet count for given movie
+            volume_window[movie]=0 # clear tweet count for current window
         window_start_time=tweet_time #window starts at this current tweet's time
         cum_x.append(str(window_start_time))
     else: # not yet 20 seconds, keep adding ratings
         curr_window_rating[earliest_movie].append(curr_tweets[earliest_movie][-1]) #save current rating
+        volume_window[earliest_movie]+=1
         #quantize follower count into weights
         if (curr_tweets[earliest_movie][2] <3): #1st quartile
             curr_window_rating_followers[earliest_movie].append(1)
@@ -150,14 +162,14 @@ for _ in range(500):
         else:
             curr_window_rating_followers[earliest_movie].append(4)
 
-    print(curr_tweets)
-    print("Avg rating: ", window_rating_avg)
-    print("current rating: ", curr_window_rating)
-    print("cum_avg: ", cum_avg)
+    # print(curr_tweets)
+    # print("Avg rating: ", window_rating_avg)
+    # print("current rating: ", curr_window_rating)
+    # print("cum_avg: ", cum_avg)
 
     #################### Plot Data ################################
     plt.clf() #clear plot contents but keep window open
-    ax = fig.add_subplot(131)
+    ax = fig.add_subplot(231)
     acc_age=['0-1', '2-4', '5-9', '10+']
 
     x_axis = np.arange(len(acc_age))
@@ -177,11 +189,11 @@ for _ in range(500):
     ax.set_ylim([0,10])
 
     #################### Plot Data 2 ################################
-    ax2 = fig.add_subplot(132)
+    ax2 = fig.add_subplot(232)
     colors=['black', 'grey', 'red', 'green', 'blue']
     width=[0.7]*len(movies)
     ax2.bar(['Batman', 'Fantastic Beasts', 'Morbius', 'Northman', 'Sonic'], window_rating_avg, color=colors, width=width)
-    ax2.set_title('Average Movie Rating in 10 Second Tumbling Window (Follower Weighted)', fontsize=10, fontweight='bold')
+    ax2.set_title('Average Movie Rating in 10 Second Tumbling Window \n (Follower Weighted)', fontsize=10, fontweight='bold')
     #ax2.text(0, 9, 'Window start time: ' + str(window_start_time))
     ax2.set_xlabel('Movie Title')
     ax2.set_ylabel('Sentiment Rating')
@@ -189,7 +201,7 @@ for _ in range(500):
 
 
     #################### Plot Data 3 ################################
-    ax3 = fig.add_subplot(133)
+    ax3 = fig.add_subplot(236)
     for idx, movie in enumerate(movies):
         ax3.plot(cum_x, cum_avg[movie], color=colors[idx], label=movie )
     ax3.set_title('Cumulative Movie Rating', fontsize=10, fontweight='bold')
@@ -202,6 +214,34 @@ for _ in range(500):
     ax3.set_xticks(np.arange(0, len(cum_x) + 1, int(len(cum_x)/3) if int(len(cum_x)/3) else 1))
     plt.xticks(rotation=10)
 
+
+    #################### Plot Data 4 ################################
+    ax4 = fig.add_subplot(235)
+    for idx, movie in enumerate(movies):
+        ax4.plot(cum_x, temp_avg[movie], color=colors[idx], label=movie )
+    ax4.set_title('Average Movie Rating in 10 Second Tumbling Window \n with Historical Data', fontsize=10, fontweight='bold')
+    #ax3.text(0, 9, 'Window start time: ' + str(window_start_time))
+    ax4.legend()
+    ax4.set_xlabel('Time')
+    ax4.set_ylabel('Sentiment Rating')
+    ax4.set_ylim([0,10])
+    #ax3.set_xticks(np.arange(0, len(cum_x) + 1, min(2, len(cum_x))))
+    ax4.set_xticks(np.arange(0, len(cum_x) + 1, int(len(cum_x)/3) if int(len(cum_x)/3) else 1))
+    plt.xticks(rotation=10)
+
+    #################### Plot Data 5 ################################
+    ax5 = fig.add_subplot(234)
+    for idx, movie in enumerate(movies):
+        ax5.plot(cum_x, volume_trend[movie], color=colors[idx], label=movie )
+    ax5.set_title('Tweet Volumes Over Time', fontsize=10, fontweight='bold')
+    ax5.legend()
+    ax5.set_xlabel('Time')
+    ax5.set_ylabel('Number of Tweets/10s')
+    #ax5.set_ylim([0,10])
+    ax5.set_xticks(np.arange(0, len(cum_x) + 1, int(len(cum_x)/3) if int(len(cum_x)/3) else 1))
+    plt.xticks(rotation=10)
+
     fig.suptitle('Tweet time: ' + earliest_tweet_time+"\n"+'Window start time: ' + str(window_start_time))
+    plt.subplots_adjust(hspace=0.3) #increase space btw top and bottom figures
     plt.pause(0.01)
 plt.show()
